5b9b65c1706b35f7dbbdd385783fbf9d79cbfbf8
Alex Patterson <admin@ajonp.com>
2019-04-29 12:21:23 -0400

    api failure, we still want firebase update

--- src/components/AddCat.js
+++ src/components/AddCat.js
@@ -3,25 +3,26 @@ import React from 'react';
 import { firestore } from '../Firebase';
 
 class AddCat extends React.Component {
-  addCatFact = () => {
-    /* The dreaded CORS, had to pass through a proxy */
-    fetch(
-      `https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`
-    )
-      .then(blob => blob.json())
-      .then(value => {
-        console.log('fetched', value);
-        firestore
-          .collection('catfacts')
-          .add({ ...value, uid: this.props.user.uid, catFactDate: new Date() })
-          .then(
-            () => {},
-            reason => {
-              alert('Must Be Logged In To Add, See Console');
-              console.log('Failed Adding Cat Fact', reason);
-            }
-          );
+  addCatFact = async () => {
+    try {
+      /* The dreaded CORS, had to pass through a proxy */
+      const blob = await fetch(
+        `https://cors-anywhere.herokuapp.com/https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`
+      );
+      const value = await blob.json();
+      await firestore
+        .collection('catfacts')
+        .add({ ...value, uid: this.props.user.uid, catFactDate: new Date() });
+    } catch (error) {
+      console.error(error);
+      await firestore.collection('catfacts').add({
+        text: `API Failed so, Random cat fact #${Math.round(
+          Math.random() * 1000000
+        )}`,
+        uid: this.props.user.uid,
+        catFactDate: new Date()
       });
+    }
   };
   render() {
     let addCatButton = null;
